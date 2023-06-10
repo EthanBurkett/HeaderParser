0d09fccdf4f387caa58167a92dbba207ced6bdd0
EthanBurkett <ethan@ethanburkett.net>
2023-06-09 22:42:02 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    ip: req.ip,
+    software: req.headers["user-agent"],
+    language: req.headers["accept-language"],
+  });
+});
+
+app.listen();
